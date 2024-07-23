a61419822c7d05b138662492b6aa1c9c5dcc7031
Nick Rabone <n.rabone72@aol.com>
2024-07-23 09:15:24 +0100

    Playing around with the previous program.

--- 11---Trigonometry-2/script.js
+++ 11---Trigonometry-2/script.js
@@ -4,7 +4,7 @@ canvas.width = 700
 canvas.height = 900
 
 ctx.lineWidth = 10
-ctx.shadowOffsetX = 2
+ctx.shadowOffsetX = 2.8
 ctx.shadowOffsetY = 2
 ctx.shadowColor = 'black'
 
@@ -27,18 +27,18 @@ class Line {
         this.x = Math.random() * this.canvas.width
         this.y = Math.random() * this.canvas.height
         this.history = [{x: this.x, y: this.y}]
-        this.lineWidth = Math.floor(Math.random() * 15 + 1)
-        this.hue = Math.floor(Math.random() * 360)
-        this.maxLength = Math.floor(Math.random() * 150 + 10)
-        this.speedX = Math.random() * 1 - 0.5
+        this.lineWidth = Math.floor(Math.random() * 4 + 9)
+        this.hue = Math.floor(Math.random() * 470)
+        this.maxLength = Math.floor(Math.random() * 163 + 10)
+        this.speedX = Math.random() * 1 - 0.7
         this.speedY = 7
-        this.lifeSpan = this.maxLength * 2
+        this.lifeSpan = this.maxLength * 2.2
         this.timer = 0
-        this.angle = 0
-        this.curve = 30
+        this.angle = 0.5
+        this.curve = 21
     }
     draw(context) {
-        context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)'
+        context.strokeStyle = 'hsl(' + this.hue + ', 65%, 43%)'
         context.lineWidth = this.lineWidth
         context.beginPath()
         context.moveTo(this.history[0].x, this.history[0].y)
@@ -51,8 +51,8 @@ class Line {
         this.timer++
         this.angle += 0.1
         if (this.timer < this.lifeSpan) {
-            this.x += Math.sin(this.angle) * this.curve
-            this.y += this.speedY + Math.random() * 20 - 10
+            this.x += Math.sin(this.angle) * this.curve * 1
+            this.y += Math.cos(this.angle) * this.curve * Math.random() * 21
             this.history.push({ x: this.x, y: this.y })
             if (this.history.length > this.maxLength) {
                 this.history.shift()

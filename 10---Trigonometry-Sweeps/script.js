3643584099fa58fa318f65472f4198809ea487aa
Nick Rabone <n.rabone72@aol.com>
2024-07-23 04:58:36 +0100

    Trigonomic sweeps project done . Project cleaned up

--- 10---Trigonometry-Sweeps/script.js
+++ 10---Trigonometry-Sweeps/script.js
@@ -21,30 +21,24 @@ gradient2.addColorStop('0.2', 'green')
 gradient2.addColorStop('0.5', 'red')
 gradient2.addColorStop('0.8', 'blue')
 
-
-const patternImage = document.getElementById('patternImage')
-const pattern1 = ctx.createPattern(patternImage, 'no-repeat')
-
-ctx.strokeStyle = gradient1
-
-
-
 class Line {
     constructor(canvas) {
         this.canvas = canvas
         this.x = Math.random() * this.canvas.width
         this.y = Math.random() * this.canvas.height
         this.history = [{x: this.x, y: this.y}]
-        this.lineWidth = Math.floor(Math.random() * 50 + 1)
+        this.lineWidth = Math.floor(Math.random() * 15 + 1)
         this.hue = Math.floor(Math.random() * 360)
         this.maxLength = Math.floor(Math.random() * 150 + 10)
         this.speedX = Math.random() * 1 - 0.5
-        this.speedY = 3
+        this.speedY = 7
         this.lifeSpan = this.maxLength * 2
         this.timer = 0
+        this.angle = 0
+        this.curve = 30
     }
     draw(context) {
-        // context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)'
+        context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)'
         context.lineWidth = this.lineWidth
         context.beginPath()
         context.moveTo(this.history[0].x, this.history[0].y)
@@ -55,8 +49,9 @@ class Line {
     }
     update() {
         this.timer++
+        this.angle += 0.1
         if (this.timer < this.lifeSpan) {
-            this.x += this.speedX + Math.random() * 20 - 10
+            this.x += Math.sin(this.angle) * this.curve
             this.y += this.speedY + Math.random() * 20 - 10
             this.history.push({ x: this.x, y: this.y })
             if (this.history.length > this.maxLength) {
